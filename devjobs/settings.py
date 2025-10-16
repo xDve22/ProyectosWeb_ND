a342dca63fa5fcfea95416ea9bd9e316fd3a3749
01001110| <a10100105555@gmail.com>
2025-10-15 22:19:36 -0500

    feat: configure database settings for different environments

--- devjobs/settings.py
+++ devjobs/settings.py
@@ -87,22 +87,22 @@ WSGI_APPLICATION = 'devjobs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+ENVIRONMENT = os.getenv('DJANGO_ENV', 'development')
 
-DATABASES = {
-   'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
-}
-
-
-# python manage.py dumpdata > backup.json
-# python manage.py loaddata backup.json
-
-
+if ENVIRONMENT == 'production':
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv('DATABASE_URL'),
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
